50216a78220c39c6479e733320282a2be423c533
daniel.xiaofei@gmail.com <xiaofei.sun@ingeek.com>
2021-07-16 23:32:26 +0800

    添加// 16进制转NSData方法

--- Example/TFThirdLib_iOS/TFViewController.m
+++ Example/TFThirdLib_iOS/TFViewController.m
@@ -23,7 +23,45 @@
 //    BOOL flag = [TFWxManager isWXAppInstalled];
 //    NSLog(@"%@", flag ? @"1" : @"0");
     
+    NSString *hexInterval = @"0x606FB3A4";
+    NSData *timestampData = [self convertHexStrToData:@"0x606FB3A4"];
+    
+    unsigned res = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexInterval];
+    [scanner scanHexInt:&res];
+    NSTimeInterval interval = (NSTimeInterval)res;
+    // Once you have an interval, use your code:
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:interval];
     BOOL result = [self matches:@"ikey"];
+}
+
+// 16进制转NSData
+- (NSData *)convertHexStrToData:(NSString *)str
+{
+    if (!str || [str length] == 0) {
+        return nil;
+    }
+    
+    NSMutableData *hexData = [[NSMutableData alloc] initWithCapacity:20];
+    NSRange range;
+    if ([str length] % 2 == 0) {
+        range = NSMakeRange(0, 2);
+    } else {
+        range = NSMakeRange(0, 1);
+    }
+    for (NSInteger i = range.location; i < [str length]; i += 2) {
+        unsigned int anInt;
+        NSString *hexCharStr = [str substringWithRange:range];
+        NSScanner *scanner = [[NSScanner alloc] initWithString:hexCharStr];
+        
+        [scanner scanHexInt:&anInt];
+        NSData *entity = [[NSData alloc] initWithBytes:&anInt length:1];
+        [hexData appendData:entity];
+        
+        range.location += range.length;
+        range.length = 2;
+    }
+    return hexData;
 }
 
 - (void)didReceiveMemoryWarning
